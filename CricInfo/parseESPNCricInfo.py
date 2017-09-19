7d9cf78d100f382ca4547b14c48c60a38853d505
Saurab Dulal <dulal.saurab@gmail.com>
2017-09-19 17:28:29 -0500

    Scraping Rus website to figure out the word count in the website

--- CricInfo/parseESPNCricInfo.py
+++ CricInfo/parseESPNCricInfo.py
@@ -1,1 +1,3 @@
+'''URL Format: http://www.espncricinfo.com/ci/engine/match/index.html?date=2017-09-15
 
+'''
---
+++ rus.py
@@ -0,0 +1,40 @@
+import urllib
+import re as regx
+from collections import Counter
+
+def word_count_in_list(revised_list): #count the words(not refined much) in a list and returns a dictonary
+    words = revised_list
+    counts = Counter(words)
+    return counts
+
+
+def parserRus():
+
+    url = "http://www.cs.memphis.edu/~vrus/teaching/ir-websearch/"
+
+    fp = urllib.request.urlopen(url) #getting html dome
+    mybytes = fp.read() #reading content
+
+    mystr = mybytes.decode("utf8") #decoding to readable ascii
+    fp.close()
+    mystr = str(mystr)
+    mystr_split = mystr.split("<!--BEGIN PAGE CONTENT") #this will get the body containt
+
+    m = (regx.sub(r'<([^>]*)>','Nepali',mystr_split[1])).strip() #replace any content "<any>" with Nepali
+    text_list = m.split('Nepali')
+    revised_list = []
+    for item in text_list:
+        text_list = item.strip()
+        if text_list !='':
+            temp = regx.split(' |\n|/',text_list) #spliting text on the basis of space and newline, we can add other spliting ws her
+            for temp_item in temp:
+                revised_list.append(temp_item)
+
+    print(word_count_in_list(revised_list)) #getting word count and printing the list
+
+
+def main():
+    parserRus()
+
+if __name__ == '__main__':
+    main()
